ebc2be1741cf4cacda92df89dc72cb4effa9c0cf
Prakhar Rawat <pxr7185@g.rit.edu>
2018-08-13 20:35:18 -0700

    added screenshots for api id 1&2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 
-from flask import Flask
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
@@ -13,17 +13,32 @@ def index():
 message = [
     {
         'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
+        'title': 'Hello World to Sauce Labs',
+        'description': 'Peanut Butter cups are the best',
+        'status': 'On route'
+    }, {
         'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
+        'title': 'This is to test the id and get_message',
+        'status': 'I forgot statuses'
     }
 ]
 
+
+@app.route('/helloworld/api/prakhar', methods=['GET'])
+def get_message():
+    return jsonify({'This is the content: ': message})
+
+
+#redirects to the messages array with id 1 only else abort with 404
+@app.route('/helloworld/api/prakhar/<int:msg_id>', methods=['GET'])
+def get_mesasge(msg_id):
+    msg = [msg for msg in message if msg['id'] == msg_id]
+    if len(msg) == 0:
+        abort(404)
+    return jsonify({'task': msg[0]})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
